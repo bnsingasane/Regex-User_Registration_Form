8c5954216b2a35646396cc0482b53b3e67fa0e51
bhagyashree98 <bnsingasane@gmail.com>
2020-07-23 00:08:22 +0530

    refactor:Added checking pattern for lastname of a User

--- user_registration.sh
+++ user_registration.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -x
-#!/bin/bash -x 
+
+#enter a valid First Name
+echo "Enter the First Name:"
+read firstname
+
+fnamepattern="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $firstname =~ $fnamepattern ]]
+then
+       echo "Valid Firstname"
+else
+       echo "Invalid Firstname"
+fi
 
 #enter a valid Last Name
 echo "Enter the Last Name:"
