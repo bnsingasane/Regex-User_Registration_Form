03e5941fe526f5cde239ee19058ff9e434ce2044
bhagyashree98 <bnsingasane@gmail.com>
2020-07-23 00:56:53 +0530

    refactor:Added should clear all the sample emails

--- user_registration.sh
+++ user_registration.sh
@@ -76,3 +76,31 @@ then
 else
 	echo "Your password should be of 8 length";
 fi
+
+<<SampleEmail
+-------Valid Emails----------
+abc@yahoo.com,
+abc-100@yahoo.com,
+abc.100@yahoo.com
+abc111@abc.com,
+abc-100@abc.net,
+abc.100@abc.com.au
+abc@1.com,
+abc@gmail.com.com
+abc+100@gmail.com
+
+------Invalid Emails (TLD - Top Level Domains)--------------
+abc – must contains “@” symbol
+abc@.com.my – tld can not start with dot “.”
+abc123@gmail.a – “.a” is not a valid tld, last tld must contains at least two characters
+abc123@.com – tld can not start with dot “.”
+abc123@.com.com – tld can not start with dot “.”
+.abc@abc.com – email’s 1st character can not start with “.”
+abc()*@gmail.com – email’s is only allow character, digit, underscore and dash
+abc@%*.com – email’s tld is only allow character and digit
+abc..2002@gmail.com – double dots “.” are not allow
+abc.@gmail.com – email’s last character can not end with dot “.”
+abc@abc@gmail.com – double “@” is not allow
+abc@gmail.com.1a -email’s tld which has two characters can not contains digit
+abc@gmail.com.aa.au - cannont have multiple email’s tld
+SampleEmail
